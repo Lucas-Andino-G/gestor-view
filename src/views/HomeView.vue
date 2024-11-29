c6b81cdf91db81ea80f47b664c9af709abffc92e
AndiNoSkill <lucasivanandinogonzalez@gmail.com>
2024-11-29 18:04:51 -0300

    Inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="" alt="Vue logo" src="../assets/penitente.jpg">
+    <HelloWorld msg="BIENVENIDOS A EL ULTIMO JUICIO DE LAS TAREAS"/>
   </div>
 </template>
 
